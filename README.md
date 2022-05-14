91768b17f3e02faef2e051b50e881f54bcf7fcaa
Kate Johnson <johnson.kat.g@gmail.com>
2022-05-13 23:59:02 -0400

    finished sample bot

---
+++ README.md
@@ -0,0 +1,5 @@
+# Uniswap Sniper Bot
+
+## Bot listens for new pool pair creation with WETH and swaps WETH amount of 0.1 for that token
+
+### Important: This is an example project and should not be used without knowledge of risks
---
+++ index.js
@@ -0,0 +1,63 @@
+const ethers = require('ethers');
+
+const addresses = {
+  WETH: '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2',
+  factory: '0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f',
+  router: '0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D',
+  recipient: 'address of recipient of funds here',
+}
+
+const mnemonic = 'mnemonic of recipient here';
+
+const provider = new ethers.providers.WebSocketProvider('Infura websocket url to mainnet');
+const wallet = ethers.Wallet.fromMnemonic(mnemonic);
+const account = wallet.connect(provider);
+const factory = new ethers.Contract(addresses.factory, ['event PairCreated(address indexed token 0, address indexed token1, address pair, uint)'], account);
+const router = new ethers.Contract(addresses.router, [
+  'function getAmountsOut(uint amountIn, address[] memory path) public view returns (uint[] memory amounts);', 'function swapExactTokensForTokens(uint amountIn, uint amountOutMin, address[] calldata path, address to, uint deadline) external returns (uint[] memory amounts);'
+], account);
+
+factory.on('PairCreated', async (token0, token1, pairAddress) => {
+  console.log(`
+  New pair detected
+  =================
+  token0: ${token0}
+  token1: ${token1}
+  pairAddress: ${pairAddress}
+  `);
+  // quote currency needs to be in WETH
+  let tokenIn, tokenOut;
+  if (token0 === WETH) {
+    tokenIn = token0,
+    tokenOut = token1;
+  }
+  if (token1 === WETH) {
+    tokenIn = token1,
+    tokenOut = token0;
+  }
+  // The quote currency is not WETH
+  if (typeof tokenIn === 'undefined') {
+    return;
+  }
+  // buy 0.1 ETH of the new token 
+  const amountIn = ethers.utils.parseUnits('0.1', 'ether');
+  const amounts = await router.getAmountsOut(amountIn, [tokenIn, tokenOut]);
+  // Execution price will be different for flexibility
+  const amountOutMin = amounts[1].sub(amounts[1].div(10));
+  console.log(`
+  Buying new token 
+  ================
+  tokenIn: ${amountIn.toString()} ${tokenIn} (WETH)
+  tokenOut: ${amountOutMin.toString()} ${tokenOut}
+  `);
+  const tx = await router.swapExactTokensForTokens(
+    amountIn,
+    amountOutMin, // give some margin
+    [tokenIn, tokenOut],
+    addresses.recipient,
+    Date.now() + 1000 * 60 * 10 // 10 min deadline
+  );
+  const receipt = await tx.wait();
+  console.log('Transaction receipt');
+  console.log(receipt);
+})
